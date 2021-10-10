cc7c2f197553d21f2b2f207bac1dd6db29799c8c
GitDogBar <87788268+DogBarCoreMVC@users.noreply.github.com>
2021-10-10 13:47:58 +0700

    Add Index.html and Index.js
    ตัวแปรต่างๆที่ใช้อยู่ในภาษา javaScript

---
+++ index.js
@@ -0,0 +1,7 @@
+const myName = 'Porawit'; //const คือตัวแปรที่ไม่สามารถเปลี่ยนแปลงค่าได้
+let Height = 160; // let คือตัวแปรที่สามารถเปลี่ยนแปลงค่าได้
+console.log(myName);
+console.log(Height);
+
+myName = 'Suchar';//error
+console.log(myName);//print error
